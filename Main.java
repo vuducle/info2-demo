5b0e65ea32a0c1a5ec05b09c48b908393b0cbabb
Vu Minh Le <vuminhle@outlook.com>
2024-04-17 20:23:49 +0200

    Change Main.java file

--- Main.java
+++ Main.java
@@ -1,5 +1,22 @@
+import java.util.concurrent.TimeUnit;
 class Main {
   public static void main(String[] args) {
+
     System.out.println("Hello World Info-2");
+
+    try {
+      while (true) {
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("Jede Sekunde wird was geprintet:");
+        meow();
+      }
+    } catch (Exception e) {
+      System.out.print(e.toString());
+    }
+
+  }
+  private static void meow() {
+
+    System.out.println("Git beste! Lab02, \uD83D\uDDFF");
   }
 }
